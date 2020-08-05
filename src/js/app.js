ccb4925f38602da66721b16b04d51495629931c2
Erick <erickson.998070@gmail.com>
2020-08-04 21:43:54 -0500

    Fetch api terminado para una divisa

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,54 @@
-var hours = document.getElementById('hora');
-var date = document.getElementById('fecha');
-// hora y dia 
-var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-var dias = new Array("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo");
+var hours = document.getElementById("hora");
+var date = document.getElementById("fecha");
+var dolar = document.getElementById("dolar");
+// hora y dia
+var meses = new Array(
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+);
+var dias = new Array(
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo"
+);
 var hoy = new Date();
 
-var fecha = dias[hoy.getDay() - 1] + ', ' + hoy.getDate() + ' de ' + meses[hoy.getMonth() - 1] + ' del ' + hoy.getFullYear();
-var hora = ' Siendo las ' + hoy.getHours() + ':' + hoy.getMinutes();
+var fecha =
+    dias[hoy.getDay() - 1] +
+    ", " +
+    hoy.getDate() +
+    " de " +
+    meses[hoy.getMonth() - 1] +
+    " del " +
+    hoy.getFullYear();
+var hora = " Siendo las " + hoy.getHours() + ":" + hoy.getMinutes();
 
+function generar() {
+    fetch("https://api.exchangeratesapi.io/latest?base=USD")
+        .then((respuesta) => respuesta.json())
+        .then((response) => {
+            dolar.innerHTML = response.rates.MXN;
+            console.log(response);
+            console.log(dolar);
+        });
+}
 
 window.onload = function() {
     hours.innerHTML = hora;
     date.innerHTML = fecha;
-
-}
+    generar();
+};
